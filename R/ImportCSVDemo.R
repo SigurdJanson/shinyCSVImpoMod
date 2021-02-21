1b183f6d88660d177605d0a76bd91a84c56cde40
SigurdJanson <emailjs@web.de>
2021-02-21 23:31:28 +0100

    Fixed example (Shiny App as example runs forever)

--- R/ImportCSVDemo.R
+++ R/ImportCSVDemo.R
@@ -2,7 +2,7 @@
 
 #' @title ImportCSVDemo - sample app to show how the package works.
 #' @export
-#' @example ImportCSVDemo()
+#' @examples \dontrun{ImportCSVDemo()}
 ImportCSVDemo <- function(){
   loc <- system.file("examples", "moduleExample", package = "shiny.CSVImport")
   shinyAppDir(loc)
