3233a0e6366383c5c8aaececcefa50c5de359b63
SigurdJanson <emailjs@web.de>
2021-03-06 01:12:50 +0100

    examples, typos, checks and documentation

---
+++ inst/examples/moduleExampleOptions/app.R
@@ -0,0 +1,29 @@
+shinyApp(
+  fluidPage(
+    title = "CSV Import Sample App",
+    ModuleImportUI("ProjectDataFile"),
+    h3("Example App Preview"),
+    tableOutput("AppOutputTest")
+  ),
+  function(input,output,session){
+    DataFile <- ModuleImportServer("ProjectDataFile",
+                                   UiLng = "de",
+                                   Options = list(
+                                     Header = TRUE,
+                                     ColSep = ";",
+                                     ThousandsSep = ".",
+                                     DecimalsSep = ",",
+                                     DateFormat = "%d.%m.%Y", # strptime() default
+                                     TimeFormat = "%k:%M", # strptime() default
+                                     Quote = "",
+                                     StringsAsFactors = FALSE
+                                   )
+
+    )
+
+    output$AppOutputTest <- renderTable({
+      need(DataFile(), "Keine Daten vorhanden")
+      return(DataFile())
+    })
+  }
+)
