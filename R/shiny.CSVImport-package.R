ee178a7c3237509731483a63d20a598250c818c4
SigurdJanson <emailjs@web.de>
2021-03-21 18:09:54 +0100

    General package documentation

---
+++ R/shiny.CSVImport-package.R
@@ -0,0 +1,13 @@
+#' @details
+#' A package supporting interactive import of CSV (DSV)  files in a shiny app.
+#' The package provides a module with an UI and server implementation. Developers simply
+#' include the package. Users can change the locale settings and see the effect of various
+#' settings in a preview. That way, they can easily import CSV fiules from various sources.
+#' @keywords internal
+"_PACKAGE"
+
+# The following block is used by usethis to automatically manage
+# roxygen namespace tags. Modify with care!
+## usethis namespace: start
+## usethis namespace: end
+NULL
