5e46f1c20c3880cdb23397d4f58d6134ec128656
SigurdJanson <emailjs@web.de>
2021-10-23 14:47:02 +0200

    Disentangled the function using a generic using UseMethod()

--- R/VerifyColSpecFormat.R
+++ R/VerifyColSpecFormat.R
@@ -13,40 +13,46 @@ lengthAllEqual <- function(x)
 #' @seealso [readr::cols_condense()]
 #' @examples
 VerifyColSpecFormat <- function(ColSpec) {
+  UseMethod("VerifyColSpecFormat")
+}
 
-  if (class(ColSpec) == "col_spec") {
-    return(ColSpec)
-  }
 
-  if (class(ColSpec) == "tbl_df") {
-    ColSpec <- vroom::spec(data.frame)
-    return(ColSpec)
-  }
 
-  if (class(ColSpec) == "list") {
-    if(names(ColSpec) == ColumnSpecificationListNames) {
-      # (Sub-) lists aren't of equal length
-      if (!lengthAllEqual(ColSpec))
-        stop("Specification suggest an ambiguous number of columns")
-
-      # Make sure that `NameInFile` are syntactically valid and ...
-      # that all missings are replaced by `Name`
-      if (isTruthy(ColSpec$NameInFile)) {
-        # NULL & NA is considered as missing
-        Missing <- !isTruthyInside(ColSpec[["NameInFile"]])
-        ColSpec$NameInFile[Missing] <- ColSpec$Name[Missing]
-        ColSpec$NameInFile <- as.list(make.names(ColSpec$NameInFile))
-      }
-
-      # Convert from list spec
-      .ColSpec <- ColSpec$Type
-      names(.ColSpec) <- ColSpec$NameInFile
-      ColSpec <- do.call(vroom::cols, .ColSpec)
-      return(ColSpec)
-    } else {
-      stop("Wrong format of column specification")
-    }
-  }
-
+VerifyColSpecFormat.default <- function(ColSpec)
   stop("Unknown format of column specification")
+
+
+VerifyColSpecFormat.col_spec <- function(ColSpec) {
+  return(ColSpec)
+}
+
+
+VerifyColSpecFormat.tbl_df <- function(ColSpec) {
+  ColSpec <- vroom::spec(ColSpec)
+  return(ColSpec)
+}
+
+VerifyColSpecFormat.list <- function(ColSpec) {
+  if(names(ColSpec) == ColumnSpecificationListNames) {
+    # (Sub-) lists aren't of equal length
+    if (!lengthAllEqual(ColSpec))
+      stop("Specification suggest an ambiguous number of columns")
+
+    # Make sure that `NameInFile` are syntactically valid and ...
+    # that all missings are replaced by `Name`
+    if (isTruthy(ColSpec$NameInFile)) {
+      # NULL & NA is considered as missing
+      Missing <- !isTruthyInside(ColSpec[["NameInFile"]])
+      ColSpec$NameInFile[Missing] <- ColSpec$Name[Missing]
+      ColSpec$NameInFile <- as.list(make.names(ColSpec$NameInFile))
+    }
+
+    # Convert from list spec
+    .ColSpec <- ColSpec$Type
+    names(.ColSpec) <- ColSpec$NameInFile
+    ColSpec <- do.call(vroom::cols, .ColSpec)
+    return(ColSpec)
+  } else {
+    stop("Wrong format of column specification")
+  }
 }
