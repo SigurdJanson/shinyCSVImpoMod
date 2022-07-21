6b14e6de1c62edd1d620d0d9eebf5bb1e0a51a2f
SigurdJanson <emailjs@web.de>
2022-07-21 23:27:16 +0200

    Added visible and enabled for each interactive row

--- tests/exploration/HandMadeTable.R
+++ tests/exploration/HandMadeTable.R
@@ -1,17 +1,36 @@
 library(shiny)
+library(shinyjs)
 
+source("../../R/PreviewRenderHelper.R")
 
 # Constant to represent table settings
 .Setting <- c(Visible = TRUE, Enabled = TRUE)
 
 
+replace_with <- function(x, i, val, name, reason = NULL, error_call = caller_env()) {
+  if (is.null(val)) {
+    return(x)
+  }
 
+  check_length(val, x, name, reason, error_call = error_call)
+  check_type(val, x, name, error_call = error_call)
+  check_class(val, x, name, error_call = error_call)
+
+  i[is.na(i)] <- FALSE
+
+  if (length(val) == 1L) {
+    x[i] <- val
+  } else {
+    x[i] <- val[i]
+  }
+
+  x
+}
 
 if_else <- function (condition, true, false, missing = NULL)
 {
   if (!is.logical(condition)) {
-    msg <- glue("`condition` must be a logical vector, not {friendly_type_of(condition)}.")
-    abort(msg)
+    abort("`condition` must be a logical vector")
   }
   out <- true[rep(NA_integer_, length(condition))]
   out <- replace_with(out, condition, true, "`true`", "length of `condition`")
@@ -44,35 +63,20 @@ shinyApp(
     print(getwd())
     DataFile <- vroom::vroom("../../inst/extdata/table.csv", delim=";", show_col_types=FALSE)
 
-    renderTableRow <- function(Row) {
-      paste0("<tr>", paste0("<td>", Row, "</td>", collapse = ""), "</tr>")
-    }
+
 
 
     #' renderRow-Functions
     #'
-    #' ```
-    #' <div class="form-group shiny-input-container">
-    #'   <label class="control-label" id="ColName 2-label" for="ColName 2">...</label>
-    #'   <input id="ColName 2" type="text" class="form-control" value="A"/>
-    #' </div>
-    #' ```
+
     #' In this case we 1. skip the label, 2. remove the class "form-group" because it
     #' just adds a bottom padding, 3.
-    renderRowTextInput <- function(ColNames, Values=NULL) {
-      .render <- function(.colname, .val)
-        div(
-          class="shiny-input-container-inline",
-          tags$input(id=.colname,
-                     type="text",
-                     class="form-control",
-                     value=.val,
-                     placeholder="Enter new label") #TODO: user `Label` argument
-        )
+    renderRowTextInput <- function(ColNames, Label=NULL, Values=NULL, Enabled=TRUE) {
+      #TODO: user `Label` argument
+      Result <- lapply(ColNames, .renderTextInput, .val="", .label=Label, .enable=Enabled)
+      # TODO: use `Values`
 
-      Result <- lapply(paste("ColName", ColNames), .render, .val="A") # TODO: use `Values`
-      Result <- lapply(Result, function(x) paste0("<td>", x, "</td>"))
-      paste("<tr>", paste(Result, collapse = ""), "</tr>")
+      .renderTableRow(Result)
     }
 
 
@@ -89,29 +93,23 @@ shinyApp(
     #'
     #' @examples
     renderRowCheckBox <- function(ColNames, Label=NULL, Values=NULL, Enabled=TRUE) {
-      .render <- function(.colname, .label, .val, .enable)
-        div(
-          class="shiny-input-container-inline",
-          div(class="checkbox",
-              tags$label(
-                tags$input(id=.colname, type="checkbox", value=.val, disabled=!.enable),
-                span(.label)
-              )
-          )
-        )
+      Result <- lapply(ColNames, .renderCheckBox, .val=TRUE, .label=Label, .enable=Enabled)
+      # TODO: use `Values`
 
-      Result <- lapply(paste("ColName", ColNames), .render, .val=TRUE, .label=Label, .enable=Enabled) # TODO: use `Values`
-      Result <- lapply(Result, function(x) paste0("<td>", x, "</td>"))
-      paste("<tr>", paste(Result, collapse = ""), "</tr>")
+      .renderTableRow(Result)
     }
 
 
 
-    renderDataPreview <- function(Data, NameEdit=.Setting, Types=.Setting, Include=.Setting) {
+    #' renderDataPreview
+    #'
+    #'
+    renderDataPreview <- function(Data, ColSpec,
+                                  NameEdit=.Setting, Types=.Setting, Include=.Setting) {
       df <- head(DataFile)
 
       if (NameEdit["Visible"]){
-        Rendered <- renderRowTextInput(colnames(df)) # TODO: Enabled
+        Rendered <- renderRowTextInput(colnames(df), "Edit", Enabled = NameEdit["Enabled"])
         HtmlNameEdit <- tags$tbody(HTML(as.character(Rendered)))
       }
       else
@@ -137,7 +135,7 @@ shinyApp(
         HtmlTypes <- HTML("")
 
 
-      Content <- HTML(base::apply(df, 1, renderTableRow))
+      Content <- HTML(base::apply(df, 1, .renderTableRow))
 
       Tbl <- withTags(
         div(
@@ -156,16 +154,16 @@ shinyApp(
       return(Tbl)
     }
 
-
+#htmlEscape
 
     output$AppOutputTest <- renderUI({
       need(DataFile, "No data available")
 
       Result <- renderDataPreview(DataFile,
                                   NameEdit=c(Visible=input$chbEditVisible,
-                                            Enabled=input$chbEditEnabled),
+                                             Enabled=input$chbEditEnabled),
                                   Types=c(Visible=input$chbTypeVisible,
-                                            Enabled=input$chbTypeEnabled),
+                                          Enabled=input$chbTypeEnabled),
                                   Include=c(Visible=input$chbIncludeVisible,
                                             Enabled=input$chbIncludeEnabled))
 
